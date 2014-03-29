10e7ece611b28ef4d6c96c4edb9a58e5316f2a97
Salvatore Laisa <moebiusmania@gmail.com>
2014-03-29 11:07:41 +0100

    add post date, nav fix

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 			<section id="corpo" class="col-md-9">
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 				<article>
+					<p><small>Scritto il <?php the_time('d F Y'); ?></small></p>
 					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 					<?php the_excerpt(); ?>
 					<button class="btn btn-default">
