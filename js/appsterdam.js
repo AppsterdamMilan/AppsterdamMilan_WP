7d6fae20d18602964366410cdc290e23c7d90c4c
Salvatore Laisa <moebiusmania@gmail.com>
2015-02-05 23:55:16 +0100

    enter animation

--- js/appsterdam.js
+++ js/appsterdam.js
@@ -2,6 +2,12 @@ function init(){
 	$('header .btn').on('click',function(){
 		$('header nav').slideToggle(400);
 	});
+
+	$.each($('#corpo article'),function(i){
+		setTimeout(function(){
+			$('#corpo article').eq(i).addClass('enter');
+		},i*200);
+	});
 }
 
 $(document).on('ready',init);
